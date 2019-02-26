e50e404eaeb632f045681bf321e4430547f921d8
Kamal Maged <Kamal_Maged@yahoo.com>
2019-02-26 12:04:54 -0500

    Initial Commit

---
+++ Uber_Bean_Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Uber_Bean_Swift
+//
+//  Created by Kamal Maged on 2019-02-26.
+//  Copyright © 2019 Kamal Maged. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
